3fb966db85bcabf7bc1d1665a23f7c32b540a893
이건호 <hoyahoya0517@naver.com>
2024-08-10 23:55:04 +0900

    verTest1.93

--- src/app/page.js
+++ src/app/page.js
@@ -25,13 +25,13 @@ export default function Home() {
       </div>
       <motion.div
         initial={{ opacity: 0, y: -500 }}
-        animate={{ opacity: 1, y: 0 }}
+        animate={{ opacity: 1, y: 0, scale: heartOn ? 1.5 : 1 }}
         transition={{ duration: 1.5, type: "spring", bounce: 0.4 }}
         className={styles.heart}
       >
         <FaHeart
           onClick={() => {
-            setHeartOn(true);
+            setHeartOn((prev) => !prev);
           }}
           size={64}
           color="red"
