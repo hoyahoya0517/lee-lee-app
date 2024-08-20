2e1d7d76fc91c15b05dae09481d19e4793952213
이건호 <hoyahoya0517@naver.com>
2024-08-21 00:02:22 +0900

    testVer4.1

--- src/app/_components/wishList/wishList.jsx
+++ src/app/_components/wishList/wishList.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "./wishList.module.css";
 import { updateMemo } from "../../../server/server";
-import { motion } from "framer-motion";
 
 export default function WishList({ wishList, menuOn }) {
   const [memo, setMemo] = useState(wishList);
@@ -24,12 +23,12 @@ export default function WishList({ wishList, menuOn }) {
     <div
       className={menuOn === "wishList" ? styles.wishListOn : styles.wishListOff}
     >
-      <motion.div drag className={styles.container}>
+      <div className={styles.container}>
         <div className={styles.top}></div>
         <div className={styles.main}>
           <textarea value={memo} onChange={handleChange} />
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 }
