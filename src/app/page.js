2707513712d6761bc16c4c36b50c7c23c15a79f7
이건호 <hoyahoya0517@naver.com>
2024-08-28 01:21:07 +0900

    ver1.4

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import { getSetting, getWishList } from "../server/server";
 import Home from "./_components/home/Home";
 
+const dynamic = "force-dynamic";
+
 export default async function Page() {
   const data = await getSetting();
   const data2 = await getWishList();
